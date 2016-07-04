f7fbf40deeff74f24574f529248d7ae6831b3d7c
kimsal <kimsalsan007@gmail.com>
2016-07-04 15:30:53 +0700

    add

---
+++ manage.py
@@ -0,0 +1,31 @@
+#from api import *
+from database import *
+import os.path as op
+import os
+import flask
+#from views import *
+from flask import abort,Flask,g, render_template,request,session,redirect,url_for,flash
+from werkzeug import secure_filename
+from wtforms import TextField, IntegerField, TextAreaField, SubmitField, RadioField,SelectField,validators, ValidationError
+import math
+from models import *
+#client
+@app.errorhandler(404)
+def page_not_found(e):
+	return "Page not found 404"
+	return render_template(template+"/404.html")
+@app.route('/')
+def index():
+	limit=30
+	jobs = Job.query.order_by(Job.id).limit(limit)
+	#return str(jobs)
+	return render_template("index.html",page_name='home',jobs=jobs)
+
+if __name__ == '__main__':
+	 app.run(debug = True)
+
+
+
+
+#replace white space:
+#http://docs.python-requests.org/en/master/user/quickstart/
---
+++ models.py
@@ -0,0 +1,18 @@
+from database import *
+from sqlalchemy.orm import relationship
+from slugify import slugify
+from wtforms.widgets import * #TextArea
+from wtforms import * #TextField, IntegerField, TextAreaField, SubmitField, RadioField,SelectField,validators, ValidationError
+import wtforms.widgets.core
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    company_name=  db.Column(db.String(500),nullable=True,unique=True)
+    location= db.Column(db.String(500),nullable=True)
+    logo=db.Column(db.String(500),nullable=True)
+    title=db.Column(db.String(500),nullable=True)
+    description=db.Column(db.Text,nullable=True)
+    requirement=db.Column(db.Text,nullable=True)
+    updated_at=db.Column(db.String(200),nullable=True)
+    url = db.Column(db.String(300),nullable=True)
